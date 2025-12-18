f2da01bd3d22e37be793214eb2f52bea3a17112b
our-deepak <deepakkumargupta184@gmail.com>
2025-12-18 23:32:56 +0530

    changed backend url for render deploy

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://beauty-store-black.vercel.app",
     // origin: "http://localhost:5173",
     credentials: true,
   })
